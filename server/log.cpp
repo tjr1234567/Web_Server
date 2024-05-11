b7962892634c6edc836d861bc79b2651dcd58439
TJR <tjr030301@163.com>
2024-05-11 16:48:26 +0800

    Impement Appender and part of Formatter

--- server/log.cpp
+++ server/log.cpp
@@ -1,6 +1,28 @@
 #include "log.h"
+#include <iostream>
 namespace server
 {
+    // Log level
+    const char *LogLevel::toString(LogLevel::Level level)
+    {
+        switch (level)
+        {
+#define XX(name)         \
+    case LogLevel::name: \
+        return #name;    \
+        break;
+            XX(DEBUG)
+            XX(INFO)
+            XX(WARN)
+            XX(ERROR)
+            XX(FATAL)
+#undef XX
+        default:
+            return "UNKNOWN";
+        }
+        return "UNKNOWN";
+    }
+    // Logger
     Logger::Logger(const std::string &name)
         : m_name(name) {}
 
@@ -51,4 +73,150 @@ namespace server
         }
     }
 
+    // LogFormatter
+    LogFormatter::LogFormatter(const std::string &pattern) : m_pattern(pattern) {}
+    std::string LogFormatter::format(LogLevel::Level level, Logevent::ptr event)
+    {
+        std::stringstream s;
+        for (auto &i : m_item)
+        {
+            i->format(s, level, event);
+        }
+        return s.str();
+    }
+    // Log tempalte: %xxx  OR  %xxx{xxx}  OR  %%
+    void LogFormatter::init()
+    {
+        // str formate type
+        std::vector<std::tuple<std::string, std::string, int>> vec;
+        std::string gstr;
+        size_t last_pos;
+
+        for (size_t i; i < m_pattern.size(); i++)
+        {
+            if (m_pattern[i] != '%')
+            {
+                gstr.append(1, m_pattern[i]);
+                continue;
+            }
+            if ((i + 1) < m_pattern.size())
+            {
+                if (m_pattern[i + 1] = '%')
+                {
+                    gstr.append(1, '%');
+                    continue;
+                }
+            }
+            size_t n = i + 1;
+            int fmt_status = 0;
+            int fmt_begin = 0;
+            std::string fmt;
+            std::string s;
+            while (n < m_pattern.size())
+            {
+                if (isspace(m_pattern[i]))
+                {
+                    break;
+                }
+                if (fmt_status == 0)
+                {
+                    if (m_pattern[n] == '{')
+                    {
+                        s = m_pattern.substr(i + 1, n - i - 1);
+                        fmt_status = 1; // decode the format
+                        n++;
+                        fmt_begin = n;
+                        continue;
+                    }
+                }
+                if (fmt_status = 1)
+                {
+                    if (m_pattern[n] == '}')
+                    {
+                        fmt = m_pattern.substr(fmt_begin + 1, n - fmt_begin - 1);
+                        fmt_status = 2;
+                        n++;
+                        continue;
+                    }
+                }
+            }
+            if (fmt_status == 0)
+            {
+                if (!gstr.empty())
+                {
+                    vec.push_back(std::make_tuple(gstr, "", 0));
+                }
+                s = m_pattern.substr(i + 1, n - i - 1);
+                vec.push_back(std::make_tuple(s, fmt, 1));
+                i = n;
+            }
+            else if (fmt_status == 1)
+            {
+                std::cout << "pattern parse error:" << m_pattern << "-" << m_pattern.substr(i) << std::endl;
+                vec.push_back(std::make_tuple("pattern parse error", fmt, 0));
+            }
+            else if (fmt_status == 2)
+            {
+                vec.push_back(std::make_tuple(s, fmt, 1));
+                i = n;
+            }
+        }
+        if (!gstr.empty())
+        {
+            vec.push_back(std::make_tuple(gstr, "", 0));
+        }
+        //%m -- message
+        //%p -- level
+        //%r -- time span from the event starts to be output
+        //%c -- log name
+        //%t -- thread id
+        //%n -- enter
+        //%d -- timestamp
+        //%f -- filename
+        //%l -- line number
+    }
+    class messageFormat : public LogFormatter::formatItem
+    {
+    public:
+        std::string format(std::ostream &os, LogLevel::Level level, Logevent::ptr event) override
+        {
+            os << event->getContent();
+        }
+    };
+    class levelFormat : public LogFormatter::formatItem
+    {
+    public:
+        std::string format(std::ostream &os, LogLevel::Level level, Logevent::ptr event) override
+        {
+            os << LogLevel::toString(level);
+        }
+    };
+
+    // LogAppender
+    // FileLogAppender
+    FileLogAppender::FileLogAppender(const std::string filename) : m_filename(filename) {}
+    void FileLogAppender::log(LogLevel::Level level, Logevent::ptr event)
+    {
+        if (level >= m_level)
+        {
+            m_filestream << m_formatter->format(level, event);
+        }
+    }
+    bool FileLogAppender::reopen()
+    {
+        if (m_filestream)
+        {
+            m_filestream.close();
+        }
+        m_filestream.open(m_filename);
+        return !!m_filestream;
+    }
+    // StdoutLogAppender
+    void StdoutLogAppender::log(LogLevel::Level level, Logevent::ptr event)
+    {
+        if (level >= m_level)
+        {
+            std::cout << m_formatter->format(level, event);
+        }
+    }
 };
--- server/log.h
+++ server/log.h
@@ -6,6 +6,8 @@
 #include <stdint.h>
 #include <memory>
 #include <list>
+#include <sstream>
+#include <fstream>
 
 namespace server
 {
@@ -14,7 +16,14 @@ namespace server
     {
     public:
         typedef std::shared_ptr<Logevent> ptr;
-        Logevent();
+        Logevent(LogLevel::Level level, const char *file, int32_t m_line, uint32_t elapse, uint32_t thread_id, uint32_t fiber_id, uint64_t time);
+        const char *getfile() const { return m_file; }
+        int32_t getLine() const { return m_line; }
+        uint32_t getElapse() const { return m_elapse; }
+        uint32_t getTime() const { return m_time; }
+        uint32_t getThreadId() const { return m_threadId; }
+        uint32_t getFiberId() const { return m_fiberId; }
+        const std::string getContent() const { return m_content; }
 
     private:
         const char *m_file = nullptr; // file name
@@ -30,12 +39,14 @@ namespace server
     public:
         enum Level
         {
+            UNKNOWN = 0,
             DEBUG = 1,
             INFO = 2,
             WARN = 3,
             ERROR = 4,
             FATAL = 5
         };
+        static const char *toString(LogLevel::Level level);
     };
     // destination of log
     class LogAppender
@@ -43,18 +54,37 @@ namespace server
     public:
         typedef std::shared_ptr<LogAppender> ptr;
         virtual ~LogAppender();
-        void log(LogLevel::Level level, Logevent::ptr event);
+        virtual void log(LogLevel::Level level, Logevent::ptr event) = 0;
+        void setFormatter(LogFormatter::ptr val) { m_formatter = val; }
+        LogFormatter::ptr getFormatter() const { return m_formatter; }
 
-    private:
+    protected:
         LogLevel::Level m_level;
+        LogFormatter::ptr m_formatter;
     };
     // formatter of log
     class LogFormatter
     {
     public:
         typedef std::shared_ptr<LogFormatter> ptr;
-        std::string formate(Logevent::ptr event);
+        std::string format(LogLevel::Level level, Logevent::ptr event);
+        LogFormatter(const std::string &pattern);
+
+    public:
+        class formatItem
+        {
+        public:
+            typedef std::shared_ptr<formatItem> ptr;
+            virtual ~formatItem() {}
+            virtual std::string format(std::ostream &os, LogLevel::Level level, Logevent::ptr event) = 0;
+        };
+        void init();
+
+    private:
+        std::string m_pattern;
+        std::vector<formatItem::ptr> m_item;
     };
+
     // outputter of log
     class Logger
     {
@@ -81,13 +111,29 @@ namespace server
         LogLevel::Level m_level;                 // log lovel
         std::list<LogAppender::ptr> m_appenders; // the list of appender
     };
+
     // Appender which present on console
     class StdoutLogAppender : public LogAppender
     {
+    public:
+        typedef std::shared_ptr<StdoutLogAppender> ptr;
+        virtual void log(LogLevel::Level level, Logevent::ptr event) override;
+
+    private:
     };
+
     // Appender which record on logFile
     class FileLogAppender : public LogAppender
     {
+    public:
+        FileLogAppender(const std::string filename);
+        typedef std::shared_ptr<FileLogAppender> ptr;
+        virtual void log(LogLevel::Level level, Logevent::ptr event) override;
+        bool reopen(); // reopen the logfile, if the file opened successed return true
+
+    private:
+        std::string m_filename;
+        std::ofstream m_filestream;
     };
 
 }
